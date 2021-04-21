64284d6703fda55b18aa547484b94bbe567da7d3
Joni De Borger <joni.de.borger@ehb.be>
2021-04-21 19:34:05 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//API-URL
+//https://data.fixer.io/api/latest?access_key=7a08874ca073ab28b507874d92019734
+
+const calculator = {
+    rates: [],
+    from: null,
+    to: null,
+    init: async function() {
+        //Voeg een API call toe, sla de rates op in this.rates
+
+        //Maak 2 instanties aan van de Currency-klasse, deze bewaar je in de variabelen this.from en this.to
+
+        //Voeg een eventListener toe voor de convertBtn
+    },
+    convert: function() {
+        console.log('Convert');
+    }
+};
+
+class Currency {
+    constructor(name, rates) {
+
+    }
+}
+
+calculator.init();
